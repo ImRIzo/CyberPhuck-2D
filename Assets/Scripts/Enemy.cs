2567b2bb4d0c61b0528b0b790e716611c7ba5e1c
Rizwan <rizowan69@gmail.com>
2020-09-30 00:18:13 +0600

    AAA

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 500;
+    int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+            DieMadafaka();
+    }
+
+    private void DieMadafaka()
+    {
+        Debug.Log("Madafaka Died");
+        animator.SetBool("EnemyDie",true);
+        Destroy(gameObject, 2f);
+    }
+}
---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    public Transform attackpoint;
+    public float attackrange = .5f;
+    public LayerMask enemylayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Attack();
+        }
+
+    }
+
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackpoint.position,attackrange,enemylayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+            enemy.GetComponent<Enemy>().TakeDamage(1);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackpoint.position, attackrange);
+    }
+}
